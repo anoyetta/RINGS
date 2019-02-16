5fa04704a20aa207f496b39c6f4b24d6fb05a729
anoyetta <anoyetta@gmail.com>
2019-02-16 11:39:42 +0900

    v0.0.0.3711-Beta

--- source/RINGS/Version.cs
+++ source/RINGS/Version.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.1.3615")]
+[assembly: AssemblyVersion("0.0.0.3711")]
 [assembly: AssemblyConfiguration("Beta")]
